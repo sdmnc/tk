43d477ffb053f2e5cce252f49f3719a579195624
sdmnc <sergedominik@gmail.com>
2023-09-11 15:37:11 +0300

    First homework for JS

---
+++ script.js
@@ -0,0 +1,15 @@
+// Home -work for lesson 13 //
+
+let country = "Moldova";
+let continent = "Europe";
+let population = 2.615;
+
+console.log("Country name is "+ country);
+console.log(country +"'s continent is "+ continent);
+console.log(country +"'s population is "+ population + " billions");
+
+let isIsland = false;
+
+let coutryDescription = country + " is located in " + continent + ", " + population + " mln people live here";
+//Should we print it in console?//
+console.log(coutryDescription);
