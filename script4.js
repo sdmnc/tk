9322b1b6a2d1e0f814ea504837de397376e4085e
sdmnc <sergedominik@gmail.com>
2023-09-21 15:07:17 +0300

    More exercizies

--- script4.js
+++ script4.js
@@ -1,40 +1,93 @@
-//While loop//
+//1 While loop//
 
-const person = { name: "Jane", age: "23", city: "LA" }
+const person = { name: "Jane", age: "23", city: "LA" };
 
 function printObj(person) {
+  let i = 0;
 
-    let i = 0
-    
-    let keys = Object.keys(person);
-    let values = Object.values(person);
-    let count = keys.length;  // or va
-    
-    while (i < count) {
-        console.log(keys[i] + ": " + values[i]);
-      
+  let keys = Object.keys(person);
+  let values = Object.values(person);
+  let count = keys.length; // or va
 
-        i++
-    }
+  while (i < count) {
+    console.log(keys[i] + ": " + values[i]);
+    i++;
+  }
 }
-printObj(person)
-//Object search//
-const person2 = { name: "John", age: "24", city: "NY" }
+printObj(person);
 
-function search (Object, key){
-for(let k in Object){
-    if(k === key){
-        console.log("true")
-        break;
-    }else{
-        console.log("false")
-       
+//2 Object search//
+const person2 = { name: "John", age: "24", city: "NY" };
+
+function searchObj(obj, key) {
+  for (let k in obj) {
+    if (k === key) {
+      return true;
     }
-    }
+  }
+  return false;
 }
-search(person2, "age")
-search(person2, "John")
-search(person, "age")
-search(person, "gender")
 
+console.log(searchObj(person2, "age"));
+console.log(searchObj(person2, "citya"));
+console.log(searchObj(person, "city"));
+console.log(searchObj(person, "color"));
 
+// 3. Combine propreties//
+let helloWorld = { prop1: "Hello", prop2: "World", prop3: "!" };
+
+function combineObject(objct) {
+  let objKeys = Object.keys(objct);
+  let result = "";
+  let i = 0;
+
+  do {
+    let currentKey = objKeys[i];
+    let currentValue = helloWorld[currentKey];
+    result += currentValue;
+    i++;
+  } while (i < objKeys.length);
+  return result;
+}
+
+console.log(combineObject(helloWorld));
+
+// 4 number of wowels//
+
+function countVowels(str) {
+  let count = 0;
+  const vowels = "aeiouAEIOU"; // To count both uppercase and lowercase vowels
+
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+// Example usage
+const input = "“Hello world, my name is Albert”";
+console.log(countVowels(input)); // Outputs: 3
+
+// Reverse string//
+
+const reversedWords = (initialString) => {
+  const initialStringArray = initialString.split('');
+
+  const resultSrtingArray = [];
+  let i = initialStringArray.length - 1;
+  do {
+    resultSrtingArray.push(initialStringArray[i]);
+    i--;
+  } while (i >= 0);
+
+  let resultString = resultSrtingArray.join('');
+
+  return resultString;
+};
+let initialString = "Hello, World!";
+console.log(reversedWords(initialString));
+
+// Palindrome//
